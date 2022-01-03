efd6c6566977e8f17c6d83f6387858e5d0a45985
ShekhSaifuddin007 <saifsabbir4@gmail.com>
2022-01-03 12:51:49 +0600

    tag fix..

--- app/Http/Controllers/MasterSetting/ProcessServiceController.php
+++ app/Http/Controllers/MasterSetting/ProcessServiceController.php
@@ -129,7 +129,7 @@ class ProcessServiceController extends Controller
             from
                 service_confiq as a
                 join
-                client_information as c on c.id = a.client_information_id
+                client_information as c on c.id = a.client_information_id and c.activity = 1
                 where a.id not in (
                     select a.service_confiq_id from maintenace_bill_ledger a JOIN maintenace_bill b ON a.maintenace_bill_id=b.id
                     where b.year_id = $request->year and b.month_id = $request->month
